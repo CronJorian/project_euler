e718fa19e87536ad76284a1b79ae4f7872cd25d7
Sebastian Schwarzer <s.schwarzer@reply.de>
2026-07-15 11:42:23 +0200

    Fix space efficient solution for problem 40

--- python/problem_40.py
+++ python/problem_40.py
@@ -10,15 +10,16 @@ def brute_force():
 
 
 def calculate_digit(index):
+  index -= 1
   digit_len = 1
   sum_len = 9 * 10**(digit_len - 1) * digit_len
-  while index > sum_len:
+  while index >= sum_len:
     digit_len += 1
     index -= sum_len
     sum_len = 9 * 10**(digit_len - 1) * digit_len
   
   skip_nums = index // digit_len
-  digit_index = index % digit_len - 1 
+  digit_index = index % digit_len
 
   target_num = 10**(digit_len - 1) + skip_nums
   target_digit = str(target_num)[digit_index]
@@ -29,25 +30,7 @@ def space_efficient_solution():
   product = 1
   for i in range(7):
     digit = calculate_digit(10**i)
-    print(digit)
     product *= digit
   return product
 
-print(calculate_digit(0))
-print(calculate_digit(1))
-print(calculate_digit(2))
-print(calculate_digit(3))
-print(calculate_digit(4))
-print(calculate_digit(5))
-print(calculate_digit(6))
-print(calculate_digit(7))
-print(calculate_digit(8))
-print(calculate_digit(9))
-print(calculate_digit(10))
-print(calculate_digit(11))
-print(calculate_digit(12))
-print(calculate_digit(13))
-print(calculate_digit(14))
-print(calculate_digit(15))
-print(calculate_digit(16))
-# print(space_efficient_solution())
+print(space_efficient_solution())
